59fd28172cfb94dc58fd859420c610dbff874616
李君 <vleboy@gmail.com>
2015-08-31 17:46:59 +0800

    原型

---
+++ 抢相因4/files/注册协议/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i],j,_(k,l,m,n,o,p,q,_(),r,_(s,t,u,v,w,_(x,y,z,A),B,null,C,D,E,D,F,G,H,null,I,J,K,L,M,N,O,J),P,_(),Q,_(),R,_(S,[_(T,U,V,W,m,X,Y,X,Z,ba,r,_(bb,_(bc,bd,be,bd),s,bf),bg,g,P,_(),bh,_())])),bi,_(),bj,_(bk,_(bl,bm)));}; 
+var b="url",c="注册协议.html",d="generationDate",e=new Date(1440526313871.98),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="page",k="packageId",l="3b0fcf90deef47cca59b008e18660a41",m="type",n="Axure:Page",o="name",p="注册协议",q="notes",r="style",s="baseStyle",t="627587b6038d43cca051c114ac41ad32",u="pageAlignment",v="near",w="fill",x="fillType",y="solid",z="color",A=0xFFFFFFFF,B="image",C="imageHorizontalAlignment",D="center",E="imageVerticalAlignment",F="imageRepeat",G="auto",H="favicon",I="sketchFactor",J="0",K="colorStyle",L="appliedColor",M="fontName",N="Applied Font",O="borderWidth",P="adaptiveStyles",Q="interactionMap",R="diagram",S="objects",T="id",U="2de4e098891a4f459b6321d9512aa913",V="label",W="",X="textArea",Y="styleType",Z="visible",ba=true,bb="size",bc="width",bd=800,be="height",bf="42ee17691d13435b8256d8d0a814778f",bg="HideHintOnFocused",bh="imageOverrides",bi="masters",bj="objectPaths",bk="2de4e098891a4f459b6321d9512aa913",bl="scriptId",bm="u707";
+return _creator();
+})());
